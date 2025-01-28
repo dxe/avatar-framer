c8496eaad87574d0c65697f7f1b7f92d474ed622
Alexander Taylor <ajxtaylor@gmail.com>
2025-01-28 18:30:45 +0000

    Update cachebusting version

--- src/js/main.js
+++ src/js/main.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function(){
       'object:rotating': updateControls,
     });
 
-    canvas.setOverlayImage("{{ 'img/frames/frame-1.png?v=4' }}" , function() {
+    canvas.setOverlayImage("{{ 'img/frames/frame-1.png?v=5' }}" , function() {
         canvas.overlayImage.scaleToWidth(canvas.getWidth())
         canvas.renderAll()
       }, {
@@ -130,7 +130,7 @@ document.addEventListener("DOMContentLoaded", function(){
   {% for image in site.static_files %}
       {% if image.path contains 'frames/' %}
           document.getElementById("{{ image.basename }}").addEventListener("click", function (){
-            changeFrame(canvas, '{{ image.path }}?v=4');
+            changeFrame(canvas, '{{ image.path }}?v=5');
           });
       {% endif %}
   {% endfor %}
